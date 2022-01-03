d80c54606e21f41ad869c4860b19065cb9e457aa
OliverSantosCode <oliversantoscode@gmail.com>
2022-01-03 15:59:13 -0300

    modified: file - index: Get

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.get('/', function(req, res){
+    res.send('Hello World!');
+});
+
 app.listen(3000, () => {
-    console.log('Running on PORT 3000 ...');
+    console.log('Running on PORT 3000; press Ctrl+C to terminate...');
 });

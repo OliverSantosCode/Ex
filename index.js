4bd913fa371e95899c34044d6d439f9571d7dce0
OliverSantosCode <oliversantoscode@gmail.com>
2022-02-10 20:38:36 -0300

    modified: file - index: EXPRESS

--- index.js
+++ index.js
@@ -1,131 +1,32 @@
 const express = require("express");
+var routes = require('./routes');
+var http = require('http');
+var path = require('path');
 
 const app = express();
 
-var bodyParser = require('body-parser');
-var methodOverride = require('method-override');
-var router = require('./routes');
-
-var myLogger = function(req, res, next) {
-    console.log('LOGGED');
-    next();
-}
-
-var requestTime = function(req, res, next) {
-    req.requestTime = Date.now();
-    next();
-}
-
-function logErrors(err, req, res, next) {
-    console.error(err.stack);
-    next(err);
-}
-
-function clientErrorHandler(err, req, res, next) {
-    if(req.xhr) {
-        res.status(500).send({ error: 'Something failed'});
-    }else {
-        next(err);
-    }
-}
-
-function errorHandler(err, req, res, next) {
-    if(res.headersSent) {
-        return next(err);
-    }
-    res.status(500);
-    res.render('error', { error: err });
-}
-
-app.set('views', './views');
+// all environments
+app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.methodOverride());
+app.use(express.session({ secret: 'your secret here '}));
+app.use(express.bodyParser());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(myLogger);
-app.use(requestTime);
-
-app.use('/user/:id', function(req, res, next) {
-    console.log('Request Type: ', req.method);
-    next();
-});
-
-app.get('/user/:id', function(req, res, next) {
-    res.send('USER');
-});
-
-app.use('/users/:id', function(req, res, next) {
-    console.log('ID: ', req.params.id);
-    next();
-}, function(req, res, next) {
-    res.send('User Info');
-});
-
-app.get('/users/:id', function(req, res, next) {
-    res.end(req.params.id);
-});
-
-app.get('/', function(req, res){
-    res.send('GET response to the homepage ' + 'Request at: ' + req.requestTime);
-});
-
-app.get('/pug', function(req, res) {
-    res.render('index', { title: 'Express with Pug', message: 'Hello there!', text: 'Testing the Pug '});
-});
-
-app.get(/z/, function(req, res) {
-    res.send('/z/');
-});
-
-app.get('/next', function(req, res, next) {
-    console.log('the response will be sent by the next function ...');
-    next();
-}, function(req, res) {
-    res.send('Hello from next');
-});
-
-var cb0 = function(req, res, next) {
-    console.log('CB0');
-    next();
+// development only
+if('development' == app.get('env')) {
+    app.use(express.errorHandler());
 }
 
-var cb1 = function(req, res, next) {
-    console.log('CB1');
-    next();
-}
+app.get('/', routes.index);
+app.get('/users', user.list);
 
-var cb2 = function(req, res) {
-    res.send('Hello from C2!');
-}
+app.use('/routes', routes);
 
-app.get('/matrix', [cb0, cb1, cb2]);
-
-app.post('/', function(req, res){
-    res.send('POST response to the homepage');
-});
-
-app.get('/matrixx', [cb0, cb1], function(req, res, next) {
-    console.log('the response will be sent the next function ...');
-    next();
-}, function (req, res) {
-    res.send('Hello from Matrix 2!')
-});
-
-app.all('/secret', function(req, res, next) {
-    console.log('Accessing the secret section ...');
-    next();
-});
-
-app.use('/routes', router);
-
-app.use(bodyParser());
-app.use(methodOverride());
-app.use(logErrors);
-app.use(clientErrorHandler);
-app.use(errorHandler);
-app.use(function(err, req, res, next) {
-    console.log(err.stack);
-    res.status(500).send('Something broke');
-});
-
-app.listen(3000, () => {
-    console.log('Running on PORT 3000; press Ctrl+C to terminate...');
+http.createServer(app).listen(app.get('port'), () => {
+    console.log('Running on PORT: ' + app.get('port') + '; press Ctrl+C to terminate...');
 });
--- routes.js
+++ routes.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
 var router = express.Router();
 
 const options = {
@@ -13,6 +16,121 @@ const options = {
         res.set('x-timestamp', Date.now());
     }
 }
+
+var myLogger = function(req, res, next) {
+    console.log('LOGGED');
+    next();
+}
+
+var requestTime = function(req, res, next) {
+    req.requestTime = Date.now();
+    next();
+}
+
+function logErrors(err, req, res, next) {
+    console.error(err.stack);
+    next(err);
+}
+
+function clientErrorHandler(err, req, res, next) {
+    if(req.xhr) {
+        res.status(500).send({ error: 'Something failed'});
+    }else {
+        next(err);
+    }
+}
+
+function errorHandler(err, req, res, next) {
+    if(res.headersSent) {
+        return next(err);
+    }
+    res.status(500);
+    res.render('error', { error: err });
+}
+
+router.use(myLogger);
+router.use(requestTime);
+
+router.use('/user/:id', function(req, res, next) {
+    console.log('Request Type: ', req.method);
+    next();
+});
+
+router.get('/user/:id', function(req, res, next) {
+    res.send('USER');
+});
+
+router.use('/users/:id', function(req, res, next) {
+    console.log('ID: ', req.params.id);
+    next();
+}, function(req, res, next) {
+    res.send('User Info');
+});
+
+router.get('/users/:id', function(req, res, next) {
+    res.end(req.params.id);
+});
+
+router.get('/', function(req, res){
+    res.send('GET response to the homepage ' + 'Request at: ' + req.requestTime);
+});
+
+router.get('/pug', function(req, res) {
+    res.render('index', { title: 'Express with Pug', message: 'Hello there!', text: 'Testing the Pug '});
+});
+
+router.get(/z/, function(req, res) {
+    res.send('/z/');
+});
+
+router.get('/next', function(req, res, next) {
+    console.log('the response will be sent by the next function ...');
+    next();
+}, function(req, res) {
+    res.send('Hello from next');
+});
+
+var cb0 = function(req, res, next) {
+    console.log('CB0');
+    next();
+}
+
+var cb1 = function(req, res, next) {
+    console.log('CB1');
+    next();
+}
+
+var cb2 = function(req, res) {
+    res.send('Hello from C2!');
+}
+
+router.get('/matrix', [cb0, cb1, cb2]);
+
+router.post('/', function(req, res){
+    res.send('POST response to the homepage');
+});
+
+router.get('/matrixx', [cb0, cb1], function(req, res, next) {
+    console.log('the response will be sent the next function ...');
+    next();
+}, function (req, res) {
+    res.send('Hello from Matrix 2!')
+});
+
+router.all('/secret', function(req, res, next) {
+    console.log('Accessing the secret section ...');
+    next();
+});
+
+router.use(bodyParser());
+router.use(methodOverride());
+router.use(logErrors);
+router.use(clientErrorHandler);
+router.use(errorHandler);
+router.use(function(err, req, res, next) {
+    console.log(err.stack);
+    res.status(500).send('Something broke');
+});
 
 router.use(express.static('public', options));
 

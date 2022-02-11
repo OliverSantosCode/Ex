8307b06e26feafa12f59aa75bf5dea94fcbc374d
OliverSantosCode <oliversantoscode@gmail.com>
2022-02-11 19:18:17 -0300

    modified: file - index: EXPRESS 4

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+var http = require('http');
 const express = require("express");
 var routes = require('./routes');
-var http = require('http');
+var user = require('./routes/user')
 var path = require('path');
+
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var methodOverride = ('method-override');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var errorHandler = require('errorhandler');
 
 const app = express();
 
@@ -9,24 +18,30 @@ const app = express();
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.methodOverride());
-app.use(express.session({ secret: 'your secret here '}));
-app.use(express.bodyParser());
-app.use(app.router);
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(session({ 
+    resave: true,
+    saveUninitialized: true,
+    secret: 'uwotm8 '}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(multer());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if('development' == app.get('env')) {
-    app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/users', user.list);
 
 app.use('/routes', routes);
 
-http.createServer(app).listen(app.get('port'), () => {
-    console.log('Running on PORT: ' + app.get('port') + '; press Ctrl+C to terminate...');
+// development only
+if('development' == app.get('env')) {
+    app.use(errorHandler());
+}
+
+var server = http.createServer(app);
+server.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });

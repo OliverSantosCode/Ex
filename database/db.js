072c9d0b088fb8d5337e8bf50a14dace723aa01a
OliverSantosCode <oliversantoscode@gmail.com>
2022-02-15 21:36:49 -0300

    modified: file - database: COUCHDB

--- database/db.js
+++ database/db.js
@@ -5,3 +5,20 @@ client.execute('select key from system.local', function(err, result) {
     if(err) throw err;
     console.log(result.rows[0]);
 });
+
+// CouchDB
+var nano = require('nano')('http://localhost:5984');
+nano.db.create('books');
+var books = nano.db.use('books');
+
+//Insert a book document in the books database
+books.insert({name: 'The Art od war'}, null, function(err, body) {
+    if(!err){
+        console.log(body);
+    }
+});
+
+// Get a list of all books
+books.list(function(err, body) {
+    console.log(body.rows);
+});

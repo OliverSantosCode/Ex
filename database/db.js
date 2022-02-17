4ed662408170e453e8f28cd119c543c60ebd4f15
OliverSantosCode <oliversantoscode@gmail.com>
2022-02-17 18:13:34 -0300

    modified: file - database: MYSQL

--- database/db.js
+++ database/db.js
@@ -35,3 +35,20 @@ db.put('name', 'LevelUP', function(err) {
         console.log('name= ' + value);
     });
 });
+
+// MySQL
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host        : 'localhost',
+    user        : 'dbuser',
+    password    : 's3kreee7'
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if(err) throw err;
+    console.log('There solution is: ', rows[0].solution);
+});
+
+connection.end();

db27caaddf7e56cde664b7e153f65d16efede370
OliverSantosCode <oliversantoscode@gmail.com>
2022-02-25 21:23:46 -0300

    modified: file - database: ELASTICSEARCH

--- database/db.js
+++ database/db.js
@@ -140,3 +140,26 @@ db.serialize(function() {
 });
 
 db.close();
+
+// ElasticSearch
+var elasticsearch = require('elasticsearch');
+var client = elasticsearch.Client({
+    host: 'localhost:9200'
+});
+
+client.search({
+    index: 'books',
+    type: 'book',
+    body: {
+        query: {
+            multi_match: {
+                query: 'express js',
+                fields: ['title', 'description']
+            }
+        }
+    }
+}).then(function(response) {
+    var hits = response.hits.hits;
+}, function(error) {
+    console.trace(error.message);
+});
